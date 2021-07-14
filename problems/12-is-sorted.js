6b2bbfdc5a34f5c0b792f29eeaf8990beb7991fc
Benjamin Rose <benthere914@gmail.com>
2021-07-14 16:58:23 -0500

    12 started

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -10,7 +10,19 @@ isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
-// your code here
+function isSorted(arr, n){
+  if (arr.length === 0) return true;
+  let el = arr.shift();
+  console.log(el)
+  // if (el > arr[0]) return false;
+  // else isSorted(arr)
+}
+
+
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
